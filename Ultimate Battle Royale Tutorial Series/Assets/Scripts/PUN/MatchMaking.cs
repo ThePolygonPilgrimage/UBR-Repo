d50896ef8ce5b53268f5eacf75fbabe21488be66
Matt Parkin <mentallic3d@gmail.com>
2020-10-05 18:41:07 -0400

    Initial Commit
    This is the first time we are uploading to github

---
+++ Ultimate Battle Royale Tutorial Series/Assets/Scripts/PUN/MatchMaking.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MatchMaking : MonoBehaviourPunCallbacks
+{
+    //Setup our instance to this script
+    public static MatchMaking instance;
+
+    public string roomNameCreated;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartMatch()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.LoadLevel(3);
+    }
+
+    public void CreateRoomOnClick()
+    {
+        string roomName = "room_ " + PhotonNetwork.Time + " _players";
+        roomNameCreated = roomName;
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        roomOptions.MaxPlayers = 13;
+        PhotonNetwork.CreateRoom(roomName, roomOptions, null);
+    }
+}
---
+++ Ultimate Battle Royale Tutorial Series/Assets/Scripts/ResetButtonImage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetButtonImage : MonoBehaviour
+{
+    public Sprite defaultImage;
+
+  public void ResetImage()
+  {
+        this.GetComponent<Image>().sprite = defaultImage;
+  }
+}
